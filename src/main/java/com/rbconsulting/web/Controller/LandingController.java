1d58532f6ec2e95067fefe6ed74fd5bfd317a950
Tang Mao <tangmao@TangdeiMac.local>
2017-06-02 19:26:52 +0200

    Basic Setup for Spring Projection

---
+++ src/main/java/com/rbconsulting/web/Controller/LandingController.java
@@ -0,0 +1,49 @@
+package com.rbconsulting.web.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+// import com.rbconsulting.web.Greeting;
+
+@Controller
+public class LandingController {
+	
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@RequestMapping("/")
+	public String landingPage(Model model) {
+		return "greetingqweqweqwd";
+	}
+
+	@RequestMapping("/index")
+	public String indexPage(Model model) {
+		return "/index";
+	}
+
+	@GetMapping("/")
+    public String home1() {
+        return "/home";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "/home";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "/admin";
+    }
+
+
+}
