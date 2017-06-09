7c528330c26b52a882edace4338b49129839e065
Mao Tang <mtang@ml905929.corpad.adbkng.com>
2017-06-09 16:01:23 +0200

    Add mysql config

--- src/main/java/com/rbconsulting/web/Bean/MentorBean.java
+++ src/main/java/com/rbconsulting/web/Bean/MentorBean.java
@@ -1,16 +1,16 @@
-package com.rbconsulting.web.Action;
+package com.rbconsulting.web.Bean;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.rbconsulting.web.Service.ArrayService;
 
 @Configuration
-public class Mentors {
+public class MentorBean {
   	
   	@Bean(name = "allMentors")
     public String[] getAllMentors() {
   		String[] mentors = {"wkf", "zyd"};
         return mentors;
     }
-  	
 }
+  	
